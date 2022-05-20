a8110476fd52303d59e931575c38c9bf92f2932f
Matijs Lode <matijs.lode@lak4.lv>
2022-05-21 02:00:29 +0300

    Created soundManager. Add enabled states. Add configurable frequencies. Add stereo.

--- src/components/MetronomeComponent.vue
+++ src/components/MetronomeComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mt-2">
     <div class="d-flex justify-content-center mb-3">
-      <h1 class="me-5">Metronome</h1>
+      <h1 class="">Metronome</h1>
     </div>
     <div class="d-flex justify-content-end">
       <div><span></span></div>
@@ -48,68 +48,109 @@
         v-model="masterVolume"
         id="masterVolumeRange"
       />
+
       <label for="accentVolumeRange" class="form-label h4"
         >Accent <small style="text-muted">{{ accentVolume }}</small></label
       >
-      <input
-        type="range"
-        class="form-range"
-        min="0"
-        max="100"
-        step="1"
-        v-model="accentVolume"
-        id="accentVolumeRange"
-      />
+      <div class="d-flex">
+        <input
+          class="form-check-input me-2"
+          type="checkbox"
+          v-model="soundManager.accent.enabled"
+        />
+        <input
+          type="range"
+          class="form-range"
+          min="0"
+          max="100"
+          step="1"
+          v-model="accentVolume"
+          id="accentVolumeRange"
+        />
+        <input class="form-control ms-2" style="width: 20%" type="number" v-model="soundManager.accent.frequency">
+      </div>
+
       <label for="quarterVolumeRange" class="form-label h4"
         >Quarter Note
         <small style="text-muted">{{ quarterVolume }}</small></label
       >
-      <input
-        type="range"
-        class="form-range"
-        min="0"
-        max="100"
-        step="1"
-        v-model="quarterVolume"
-        id="quarterVolumeRange"
-      />
+      <div class="d-flex">
+        <input
+          class="form-check-input me-2"
+          type="checkbox"
+          v-model="soundManager.quarter.enabled"
+        />
+        <input
+          type="range"
+          class="form-range"
+          min="0"
+          max="100"
+          step="1"
+          v-model="quarterVolume"
+          id="quarterVolumeRange"
+        />
+      </div>
+
       <label for="eighthVolumeRange" class="form-label h4"
         >Eighth Note <small style="text-muted">{{ eighthVolume }}</small></label
       >
-      <input
-        type="range"
-        class="form-range"
-        min="0"
-        max="100"
-        step="1"
-        v-model="eighthVolume"
-        id="eighthVolumeRange"
-      />
+      <div class="d-flex">
+        <input
+          class="form-check-input me-2"
+          type="checkbox"
+          v-model="soundManager.eighth.enabled"
+        />
+        <input
+          type="range"
+          class="form-range"
+          min="0"
+          max="100"
+          step="1"
+          v-model="eighthVolume"
+          id="eighthVolumeRange"
+        />
+      </div>
+
       <label for="sixteenthVolumeRange" class="form-label h4"
         >Sixteenth Note
         <small style="text-muted">{{ sixTeenthVolume }}</small></label
       >
-      <input
-        type="range"
-        class="form-range"
-        min="0"
-        max="100"
-        step="1"
-        v-model="sixTeenthVolume"
-        id="sixteenthVolumeRange"
-      />
+      <div class="d-flex">
+        <input
+          class="form-check-input me-2"
+          type="checkbox"
+          v-model="soundManager.sixTeenth.enabled"
+        />
+        <input
+          type="range"
+          class="form-range"
+          min="0"
+          max="100"
+          step="1"
+          v-model="sixTeenthVolume"
+          id="sixteenthVolumeRange"
+        />
+      </div>
+
       <label for="tripletVolumeRange" class="form-label h4"
         >Triplet <small style="text-muted">{{ tripletVolume }}</small></label
       >
-      <input
-        type="range"
-        class="form-range"
-        min="0"
-        max="100"
-        step="1"
-        v-model="tripletVolume"
-        id="tripletVolumeRange"
-      />
+      <div class="d-flex">
+        <input
+          class="form-check-input me-2"
+          type="checkbox"
+          v-model="soundManager.triplet.enabled"
+        />
+        <input
+          type="range"
+          class="form-range"
+          min="0"
+          max="100"
+          step="1"
+          v-model="tripletVolume"
+          id="tripletVolumeRange"
+        />
+      </div>
     </div>
     <div>
       <h2>Song Library</h2>
@@ -129,13 +170,36 @@ export default {
       //currentTwelveletNote,
       tempo: 120.0,
       meter: 4,
-      volumes: {
-        master: 0.5,
-        accent: 1,
-        quarter: 0.75,
-        eighth: 0,
-        sixTeenth: 0,
-        triplet: 0,
+      soundManager: {
+        master: {
+          volume: 0.5,
+          enabled: true,
+        },
+        accent: {
+          volume: 1,
+          enabled: true,
+          frequency: 880,
+        },
+        quarter: {
+          volume: 0.75,
+          enabled: true,
+          frequency: 440,
+        },
+        eighth: {
+          volume: 0,
+          enabled: true,
+          frequency: 440,
+        },
+        sixTeenth: {
+          volume: 0,
+          enabled: true,
+          frequency: 220,
+        },
+        triplet: {
+          volume: 0,
+          enabled: true,
+          frequency: 300,
+        },
       },
       lookahead: 25.0,
       scheduleAheadTime: 0.1,
@@ -202,31 +266,38 @@ export default {
 
       let osc = this.audioContext.createOscillator();
       let gainNode = this.audioContext.createGain();
+      let panNode = this.audioContext.createStereoPanner();
+
+      // this.audioContext.
 
       osc.connect(gainNode);
-      gainNode.connect(this.audioContext.destination);
+      gainNode.connect(panNode);
+      panNode.connect(this.audioContext.destination);
 
-      if (beatNumber % this.maxBeats === 0) {
-        if (this.accentVolume > 0.25) {
-          osc.frequency.value = 880.0;
-          gainNode.gain.value = this.calcVolume(this.accentVolume);
-        } else {
-          osc.frequency.value = 440.0;
-          gainNode.gain.value = this.calcVolume(this.quarterVolume);
-        }
-      } else if (beatNumber % 12 === 0) {
+      if (beatNumber % this.maxBeats === 0 && this.soundManager.accent.enabled) {
+        osc.frequency.value = this.soundManager.accent.frequency;
+        gainNode.gain.value = this.calcVolume(this.accentVolume);
+        
+        // if (this.accentVolume > 0.25) {
+        //   osc.frequency.value = this.soundManager.accent.frequency;
+        //   gainNode.gain.value = this.calcVolume(this.accentVolume);
+        // } else {
+        //   osc.frequency.value = this.soundManager.quarter.frequency;
+        //   gainNode.gain.value = this.calcVolume(this.quarterVolume);
+        // }
+      } else if (beatNumber % 12 === 0 && this.soundManager.quarter.enabled) {
         // quarter notes = medium pitch
-        osc.frequency.value = 440.0;
+        osc.frequency.value = this.soundManager.quarter.frequency;
         gainNode.gain.value = this.calcVolume(this.quarterVolume);
-      } else if (beatNumber % 6 === 0) {
-        osc.frequency.value = 440.0;
+      } else if (beatNumber % 6 === 0 && this.soundManager.eighth.enabled) {
+        osc.frequency.value = this.soundManager.eighth.frequency;
         gainNode.gain.value = this.calcVolume(this.eighthVolume);
-      } else if (beatNumber % 4 === 0) {
-        osc.frequency.value = 300.0;
+      } else if (beatNumber % 4 === 0 && this.soundManager.triplet.enabled) {
+        osc.frequency.value = this.soundManager.triplet.frequency;
         gainNode.gain.value = this.calcVolume(this.tripletVolume);
-      } else if (beatNumber % 3 === 0) {
+      } else if (beatNumber % 3 === 0 && this.soundManager.sixTeenth.enabled) {
         // other 16th notes = low pitch
-        osc.frequency.value = 220.0;
+        osc.frequency.value = this.soundManager.sixTeenth.frequency;
         gainNode.gain.value = this.calcVolume(this.sixTeenthVolume);
       } else {
         gainNode.gain.value = 0; // keep the remaining twelvelet notes inaudible
@@ -245,50 +316,50 @@ export default {
     },
     masterVolume: {
       get() {
-        return this.volumes.master * 100;
+        return this.soundManager.master.volume * 100;
       },
       set(volume) {
-        this.volumes.master = volume / 100;
+        this.soundManager.master.volume = volume / 100;
       },
     },
     accentVolume: {
       get() {
-        return this.volumes.accent * 100;
+        return this.soundManager.accent.volume * 100;
       },
       set(volume) {
-        this.volumes.accent = volume / 100;
+        this.soundManager.accent.volume = volume / 100;
       },
     },
     quarterVolume: {
       get() {
-        return this.volumes.quarter * 100;
+        return this.soundManager.quarter.volume * 100;
       },
       set(volume) {
-        this.volumes.quarter = volume / 100;
+        this.soundManager.quarter.volume = volume / 100;
       },
     },
     eighthVolume: {
       get() {
-        return this.volumes.eighth * 100;
+        return this.soundManager.eighth.volume * 100;
       },
       set(volume) {
-        this.volumes.eighth = volume / 100;
+        this.soundManager.eighth.volume = volume / 100;
       },
     },
     tripletVolume: {
       get() {
-        return this.volumes.triplet * 100;
+        return this.soundManager.triplet.volume * 100;
       },
       set(volume) {
-        this.volumes.triplet = volume / 100;
+        this.soundManager.triplet.volume = volume / 100;
       },
     },
     sixTeenthVolume: {
       get() {
-        return this.volumes.sixTeenth * 100;
+        return this.soundManager.sixTeenth.volume * 100;
       },
       set(volume) {
-        this.volumes.sixTeenth = volume / 100;
+        this.soundManager.sixTeenth.volume = volume / 100;
       },
     },
   },
